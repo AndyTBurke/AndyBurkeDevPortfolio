60f61c1ed4043a0b6b3e2365bbba3abaa4ab683c
AndyTBurke <107972213+AndyTBurke@users.noreply.github.com>
2022-09-05 21:30:14 -0500

    updates tp Projects page
    added links to stock quote gen and quiz game

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -8,19 +8,25 @@ export default function Projects() {
                     <h2>CompSci Quiz</h2>
                     <img className="proj-img" src="src\assets\images\CompSci-project.JPG"></img>
                     <p className="proj-description">Using the Open Trivia API, I've created a fun little quiz app. Test your CompSci knowledge!</p>
-                    <p>LIVE PROJECT || VIEW CODE</p>
+                    <p><a href="https://aburke-compsci-quiz.netlify.app"
+                        target="_blank">LIVE PROJECT</a> || <a href="https://github.com/AndyTBurke/Comp-Sci-Quiz"
+                        target="_blank">VIEW CODE</a></p>
                 </div>
                 <div className="project">
                     <h2>Pomodoro Timer</h2>
                     <img className="proj-img" src="src\assets\images\CompSci-project.JPG"></img>
                     <p className="proj-description">Using the Open Trivia API, I've created a fun little quiz app. Test your CompSci knowledge!</p>
-                    <p>LIVE PROJECT || VIEW CODE</p>
+                    <p><a href=""
+                        target="_blank">LIVE PROJECT</a> || <a href=""
+                        target="_blank">VIEW CODE</a></p>
                 </div>
                 <div className="project">
                     <h2>Stock 'Quote' Genrator</h2>
-                    <img className="proj-img" src="src\assets\images\CompSci-project.JPG"></img>
-                    <p className="proj-description">Using the Open Trivia API, I've created a fun little quiz app. Test your CompSci knowledge!</p>
-                    <p>LIVE PROJECT || VIEW CODE</p>
+                    <img className="proj-img" src="src\assets\images\QuoteGen-project.JPG"></img>
+                    <p className="proj-description">A simple implimentation of React. Quotes about investing are so good, I wanted to collect them.</p>
+                    <p><a href="https://aburke-stock-quotations.netlify.app/"
+                        target="_blank">LIVE PROJECT</a> || <a href="https://github.com/AndyTBurke/Stock-Quotation-Generator"
+                        target="_blank">VIEW CODE</a></p>
                 </div>
             </div>
         </div>
