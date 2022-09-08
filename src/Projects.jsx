e12f468867a249c67ea7b033e0ba9055df701225
AndyTBurke <107972213+AndyTBurke@users.noreply.github.com>
2022-09-07 21:24:25 -0500

    netlify doesn't like jpegs
    removed jpegs entirely with this push, will have to replace

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,7 +1,4 @@
 import React from "react"
-import quoteGenImg from "./assets/images/QuoteGen-project.jpg"
-import compSciQuizImg from "./assets/images/CompSci-project.jpg"
-import pomodoroImg from "./assets/images/Pomodoro-img.jpg"
 
 export default function Projects() {
     return (
@@ -10,7 +7,7 @@ export default function Projects() {
                 <div className="project">
                     <h2>CompSci Quiz</h2>
                     <a href="https://aburke-compsci-quiz.netlify.app" target="_blank">
-                    <img className="proj-img" src={compSciQuizImg}></img>
+                    <img className="proj-img" src=""></img>
                     </a>
                     <p className="proj-description">Using the Open Trivia API, I've created a fun little quiz app. Test your CompSci knowledge!</p>
                     <p><a href="https://aburke-compsci-quiz.netlify.app"
@@ -20,7 +17,7 @@ export default function Projects() {
                 <div className="project">
                     <h2>Pomodoro Timer</h2>
                     <a href="https://aburke-pomodoro.netlify.app/" target="_blank">
-                    <img className="proj-img" src={pomodoroImg}></img>
+                    <img className="proj-img" src=""></img>
                     </a>
                     <p className="proj-description">A productivity device. Set a session time for studying or working, then set a break time. Repeat.</p>
                     <p><a href="https://aburke-pomodoro.netlify.app/"
@@ -30,7 +27,7 @@ export default function Projects() {
                 <div className="project" target="_blank">
                     <h2>Stock 'Quote' Genrator</h2>
                     <a href="https://aburke-stock-quotations.netlify.app/" target="_blank">
-                    <img className="proj-img" src={quoteGenImg}></img>
+                    <img className="proj-img" src=""></img>
                     </a>
                     <p className="proj-description">A simple implimentation of React. Quotes about investing are so good, I wanted to collect them.</p>
                     <p><a href="https://aburke-stock-quotations.netlify.app/"
